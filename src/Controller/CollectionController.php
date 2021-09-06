f068bab75aa6818c62cc032bf48040630d1aa482
oulfi <j.banon01@gmail.com>
2021-09-06 14:20:52 +0200

    contributors done

--- src/Controller/CollectionController.php
+++ src/Controller/CollectionController.php
@@ -5,15 +5,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+/**
+ * @Route("/collection", name="collection")
+ */
 class CollectionController extends AbstractController
 {
     /**
-     * @Route("/collection", name="collection")
+     * @Route("/", name="_index")
      */
     public function index(): Response
     {
         return $this->render('collection/index.html.twig', [
+
+        ]);
+    }
+
+    /**
+     * @Route ("/modal", name="_modal")
+     */
+    public function modal(): Response
+    {
+        return $this->render('collection/_modal.html.twig', [
 
         ]);
     }
