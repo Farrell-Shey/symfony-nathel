e69442111a936e1c3a74ca6510ff01a33cfe2125
Philippe <roger.philippe.dev@gmail.com>
2021-09-30 16:59:39 +0200

    collection page with controller route https://127.0.0.1:8000/collection

--- src/Controller/CollectiponPageController.php
+++ src/Controller/CollectiponPageController.php
@@ -5,18 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-/**
- * @Route("/collection", name="collection")
- */
-class CollectionController extends AbstractController
+
+class CollectiponPageController extends AbstractController
 {
     /**
-     * @Route("/", name="_index")
+     * @Route("/collection", name="collectipon_page")
      */
     public function index(): Response
     {
-        return $this->render('collection.html.twig', [
-
+        return $this->render('/page/collection-page.html.twig', [
+            'controller_name' => 'CollectiponPageController',
         ]);
     }
 }
