3d8e5056c1c3bf18a6b53c9b9f1987675ee32cd4
Philippe <roger.philippe.dev@gmail.com>
2021-10-04 16:10:30 +0200

    modification of the main class and javascripts contributorToggle.js

--- assets/contributorToggle.js
+++ assets/contributorToggle.js
@@ -1,7 +1,9 @@
-document.addEventListener("DOMContentLoaded", function(e){
-  function contributorToggle() {
-    const dropdown = document.getElementById("contributors");
-    dropdown.classList.toggle('toggle-fit-content');
-  }
-})
+document.addEventListener("DOMContentLoaded", function (e) { })
+dropdown.addEventListener("click", contributorToggle);
 
+function contributorToggle() {
+  var dropdown = document.getElementById("contributors");
+  var up = document.getElementById("dropdown");
+  dropdown.classList.toggle("toggle-fit-content");
+  up.classList.toggle('btn-rotate');
+}
